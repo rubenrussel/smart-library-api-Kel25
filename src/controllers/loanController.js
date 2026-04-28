493a7829e919a38b3fb41127bcf7c557ad760cce
ruben russel <82746213+rubenrussel@users.noreply.github.com>
2026-04-28 17:32:48 +0700

    Refactor getTopBorrowers method and enhance responses

--- src/controllers/loanController.js
+++ src/controllers/loanController.js
@@ -1,6 +1,18 @@
 import { LoanModel } from '../models/loanModel.js';
 
 export const LoanController = {
+    getTopBorrowers: async (req, res) => {
+        try {
+            const borrowers = await LoanModel.getTopBorrowers();
+            res.status(200).json({
+                message: "Top 3 peminjam buku berhasil diambil",
+                data: borrowers
+            });
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    },
+
     createLoan: async (req, res) => {
         const { book_id, member_id, due_date } = req.body;
         try {
@@ -14,19 +26,13 @@ export const LoanController = {
         }
     },
 
-    getTopBorrowers: async (req, res) => {
-        try {
-            const borrowers = await LoanModel.getTopBorrowers();
-            res.status(200).json(borrowers);
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
-    },
-
     getLoans: async (req, res) => {
         try {
             const loans = await LoanModel.getAllLoans();
-            res.status(200).json(loans);
+            res.status(200).json({
+                message: "Seluruh data peminjaman berhasil diambil",
+                data: loans
+            });
         } catch (error) {
             res.status(500).json({ error: error.message });
         }
