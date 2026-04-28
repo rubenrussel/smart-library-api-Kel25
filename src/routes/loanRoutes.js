aa25a910151d53cb4177740ccff7ce474d699a3d
ruben russel <82746213+rubenrussel@users.noreply.github.com>
2026-04-28 16:19:05 +0700

    Update loanRoutes.js

--- src/routes/loanRoutes.js
+++ src/routes/loanRoutes.js
@@ -1,17 +1,10 @@
 import express from 'express'; 
-
 import { LoanController } from '../controllers/loanController.js'; 
-
- 
 
 const router = express.Router(); 
 
- 
-
+router.get('/top-borrowers', LoanController.getTopBorrowers); 
 router.get('/', LoanController.getLoans); 
-
 router.post('/', LoanController.createLoan); 
 
- 
-
-export default router; 
+export default router;
