a63bf0e4d6b2f5d7fc30fe959111b7224604830c
ruben russel <82746213+rubenrussel@users.noreply.github.com>
2026-04-28 17:04:16 +0700

    Format loanRoutes.js for consistency

--- src/routes/loanRoutes.js
+++ src/routes/loanRoutes.js
@@ -1,10 +1,10 @@
-import express from 'express'; 
-import { LoanController } from '../controllers/loanController.js'; 
+import express from 'express';
+import { LoanController } from '../controllers/loanController.js';
 
-const router = express.Router(); 
+const router = express.Router();
 
-router.get('/top-borrowers', LoanController.getTopBorrowers); 
-router.get('/', LoanController.getLoans); 
-router.post('/', LoanController.createLoan); 
+router.get('/top-borrowers', LoanController.getTopBorrowers);
+router.get('/', LoanController.getLoans);
+router.post('/', LoanController.createLoan);
 
 export default router;
